3b5da9f9a917db5fbd4b55d87c7f4e480ffdb4a3
Cameron West <camonater62@gmail.com>
2022-12-14 21:36:08 -0800

    Add (non functional) vechicle physics

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import WebGL from 'three/examples/jsm/capabilities/WebGL.js'
 import * as CANNON from 'cannon-es'
+import WebGL from 'three/examples/jsm/capabilities/WebGL.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js' 
 
 if (!WebGL.isWebGLAvailable()) {
     const warning = WebGL.getErrorMessage();
@@ -27,10 +28,18 @@ let world;
 let body;
 initCannon();
 
+// Car variables
+let carModel;
+let car;
+let wheelModels;
+let wheels;
+initCar();
+
 function initThree() {
     // Camera
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 5;
+    camera.position.y = 5;
 
     // Scene 
     scene = new THREE.Scene();
@@ -43,12 +52,26 @@ function initThree() {
 
     window.addEventListener('resize', onWindowResize);
 
-    // Box
-    const geometry = new THREE.BoxGeometry(2, 2, 2);
-    const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
+    // Orbit controls
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.target.set(0, 0, 0);
+    controls.update();
 
-    mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
+    // Sun 
+    {
+        const color = 0xffffff;
+        const intensity = 1;
+        const sunLight = new THREE.DirectionalLight(color, intensity);
+        sunLight.position.set(0, 100, 0);
+        sunLight.castShadow = true;
+        scene.add(sunLight);
+    }
+    {
+        const color = 0xffffff;
+        const intensity = 1;
+        const light = new THREE.AmbientLight(color, intensity);
+        scene.add(light);
+    }
 }
 
 function onWindowResize() {
@@ -66,11 +89,68 @@ function initCannon() {
         mass: 1,
     });
     body.addShape(shape);
-    body.angularVelocity.set(0, 10, 0);
-    body.angularDamping = 0.5;
     world.addBody(body);
 }
 
+function initCar() {
+    // WebGL
+    const gltfLoader = new GLTFLoader();
+    const url = 'models/Toyota AE86.glb';
+    gltfLoader.load(url, (gltf) => {
+        const root = gltf.scene;
+        root.position.set(2, 0, 8);
+        root.rotation.set(0, Math.PI, 0);
+        root.scale.set(0.3, 0.3, 0.3);
+        scene.add(root);
+        root.castShadow = true;
+        root.receiveShadow = true;
+        carModel = root;
+        wheelModels = root.getObjectByName('Car');
+    });
+
+    // Build the car chassis
+    const chassisShape = new CANNON.Box(new CANNON.Vec3(2, 0.5, 1))
+    const chassisBody = new CANNON.Body({ mass: 150 })
+    chassisBody.addShape(chassisShape)
+    chassisBody.position.set(0, 4, 0)
+    chassisBody.angularVelocity.set(0, 0.5, 0)
+
+    // Create the vehicle
+    const vehicle = new CANNON.RaycastVehicle({
+      chassisBody,
+    })
+
+    const wheelOptions = {
+      radius: 0.5,
+      directionLocal: new CANNON.Vec3(0, -1, 0),
+      suspensionStiffness: 30,
+      suspensionRestLength: 0.3,
+      frictionSlip: 1.4,
+      dampingRelaxation: 2.3,
+      dampingCompression: 4.4,
+      maxSuspensionForce: 100000,
+      rollInfluence: 0.01,
+      axleLocal: new CANNON.Vec3(0, 0, 1),
+      chassisConnectionPointLocal: new CANNON.Vec3(-1, 0, 1),
+      maxSuspensionTravel: 0.3,
+      customSlidingRotationalSpeed: -30,
+      useCustomSlidingRotationalSpeed: true,
+    }
+
+    wheelOptions.chassisConnectionPointLocal.set(-1, 0, 1)
+    vehicle.addWheel(wheelOptions)
+
+    wheelOptions.chassisConnectionPointLocal.set(-1, 0, -1)
+    vehicle.addWheel(wheelOptions)
+
+    wheelOptions.chassisConnectionPointLocal.set(1, 0, 1)
+    vehicle.addWheel(wheelOptions)
+
+    wheelOptions.chassisConnectionPointLocal.set(1, 0, -1)
+    vehicle.addWheel(wheelOptions)
+
+    car = vehicle;
+}
 
 function animate() {
     requestAnimationFrame(animate);
@@ -79,10 +159,27 @@ function animate() {
     world.fixedStep();
 
     // Copy coordinates from cannon.js to three.js
-    mesh.position.copy(body.position);
-    mesh.quaternion.copy(body.quaternion);
+    if (car) {
+        const body = car.chassisBody;
+        carModel.position.copy(body.position);
+        carModel.quaternion.copy(body.quaternion);
+    }
+    
 
     renderer.render(scene,camera);
 }
 
 animate();
+
+// Debug helper for 3D objects
+function dumpObject(obj, lines = [], isLast = true, prefix = '') {
+    const localPrefix = isLast ? '└─' : '├─';
+    lines.push(`${prefix}${prefix ? localPrefix : ''}${obj.name || '*no-name*'} [${obj.type}]`);
+    const newPrefix = prefix + (isLast ? '  ' : '│ ');
+    const lastNdx = obj.children.length - 1;
+    obj.children.forEach((child, ndx) => {
+        const isLast = ndx === lastNdx;
+        dumpObject(child, lines, isLast, newPrefix);
+    });
+    return lines;
+}
